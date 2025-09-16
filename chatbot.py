ee388a1571b8fb9cd9b15538de06104e72918112
champ-sk <shubhamkumar3183@gmail.com>
2025-09-16 21:24:22 +0530

    creating the CHatbot

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+model =  ChatOpenAI()   
+
+chat_history = []
+
+while True:
+    user_input = input("You: ")
+    chat_history.append(user_input)
+    if user_input == "exit":
+        break
+    response =model.invoke(chat_history)
+    chat_history.append(response.content)
+    print("Bot:", response.content)
+
+print(chat_history)    
